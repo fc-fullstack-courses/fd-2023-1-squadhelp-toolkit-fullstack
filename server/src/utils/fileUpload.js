0f54bead5659bb07ecddb5e6e113250ab57bc02b
Kyrylo <fresh.js.ftd.@gmail.com>
2023-09-25 15:56:03 +0300

    Separate contest files from avatar images

--- server/src/utils/fileUpload.js
+++ server/src/utils/fileUpload.js
@@ -2,30 +2,50 @@ const fs = require('fs');
 const path = require('path');
 const multer = require('multer');
 const env = process.env.NODE_ENV || 'development';
-const devFilePath = path.resolve(__dirname, '..', '..', '..', 'public/images');
+const devFilePath = path.resolve(__dirname, '..', '..', '..', 'public/');
 
 const filePath = env === 'production'
-  ? '/var/www/html/images/'
+  ? '/var/www/html/'
   : devFilePath;
 
-if (!fs.existsSync(filePath)) {
-  fs.mkdirSync(filePath, {
+const imagesFilePath = `${filePath}/images`;
+
+const contestsFilePath = `${filePath}/contests`;
+
+if (!fs.existsSync(imagesFilePath)) {
+  fs.mkdirSync(imagesFilePath, {
     recursive: true,
   });
 }
 
-const storageContestFiles = multer.diskStorage({
+if (!fs.existsSync(contestsFilePath)) {
+  fs.mkdirSync(contestsFilePath, {
+    recursive: true,
+  });
+}
+
+const storageImageFiles = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, filePath);
+    cb(null, imagesFilePath);
   },
   filename(req, file, cb) {
     cb(null, Date.now() + file.originalname);
   },
 });
 
-const multerFilesMW = multer({ storage: storageContestFiles });
+const storageContestFiles = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, contestsFilePath);
+  },
+  filename(req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
 
-module.exports.uploadAvatar = multerFilesMW.single('file');
+
+module.exports.uploadAvatar = multer({ storage: storageImageFiles }).single('file');
+
+const multerFilesMW = multer({ storage: storageContestFiles });
 
 module.exports.uploadContestFiles = multerFilesMW.array('files', 3);
 
