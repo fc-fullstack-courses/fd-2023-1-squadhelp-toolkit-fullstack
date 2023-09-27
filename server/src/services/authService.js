5119175f067abf6ecf4c2817f9d69e3617d81f6b
Kyrylo <fresh.js.ftd.@gmail.com>
2023-09-27 15:08:50 +0300

    Fix token payload

--- server/src/services/authService.js
+++ server/src/services/authService.js
@@ -4,9 +4,15 @@ const { RefreshToken } = require('../models');
 module.exports.createSession = async (user) => {
 
   const tokenPayload = {
-    id: user.id,
+    firstName: user.firstName,
+    userId: user.id,
+    role: user.role,
+    lastName: user.lastName,
+    avatar: user.avatar,
+    displayName: user.displayName,
+    balance: user.balance,
     email: user.email,
-    role: user.role
+    rating: user.rating,
   }
 
   const tokenPair = await tokenService.generateTokenPair(tokenPayload);
@@ -24,9 +30,15 @@ module.exports.refreshSession = async (refreshTokenInstance) => {
   const user = await refreshTokenInstance.getUser();
 
   const tokenPayload = {
-    id: user.id,
+    firstName: user.firstName,
+    userId: user.id,
+    role: user.role,
+    lastName: user.lastName,
+    avatar: user.avatar,
+    displayName: user.displayName,
+    balance: user.balance,
     email: user.email,
-    role: user.role
+    rating: user.rating,
   }
 
   const tokenPair = await tokenService.generateTokenPair(tokenPayload);
