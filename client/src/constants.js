9cf70939c4e5f45f3e289a203ab183f5ce1005e9
Kyrylo <fresh.js.ftd.@gmail.com>
2023-09-27 16:13:47 +0300

    Change token constant name

--- client/src/constants.js
+++ client/src/constants.js
@@ -16,7 +16,7 @@ export default {
   STATIC_IMAGES_PATH: '/staticImages/',
   ANONYM_IMAGE_PATH: '/staticImages/anonym.png',
   BASE_URL: `http://${serverIP}:${serverPort}/`,
-  ACCESS_TOKEN: 'accessToken',
+  REFRESH_TOKEN: 'refreshToken',
   publicImagesURL:
     env === 'production'
       ? `http://${serverIP}:80/images/`
