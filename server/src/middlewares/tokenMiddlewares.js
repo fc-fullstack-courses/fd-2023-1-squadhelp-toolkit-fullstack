0aad2c58100f34e547516b85c897c0f1e49922ea
Kyrylo <fresh.js.ftd.@gmail.com>
2023-09-27 15:14:10 +0300

    Add token validation middlewares

---
+++ server/src/middlewares/tokenMiddlewares.js
@@ -0,0 +1,49 @@
+const tokenService = require('../services/tokenService');
+const { RefreshToken } = require('../models');
+const TokenError = require('../errors/TokenError');
+
+module.exports.checkAccessToken = async (req, res, next) => {
+  try {
+    const {
+      headers: { authorization }
+    } = req;
+
+    const [tokenType, accessToken] = authorization.split(' ');
+
+
+    if (!accessToken) {
+      return next(TokenError('Access token required'));
+    }
+
+    req.tokenData = await tokenService.verifyAccessToken(accessToken);
+
+    next();
+
+  } catch (error) {
+    next(error)
+  }
+}
+
+module.exports.checkRefreshToken = async (req, res, next) => {
+  try {
+    const { body: { refreshToken } } = req;
+
+    await tokenService.verifyRefreshToken(refreshToken);
+
+    const refreshTokenInstance = await RefreshToken.findOne({
+      where: {
+        token: refreshToken
+      }
+    });
+
+    if (!refreshTokenInstance) {
+      return next(new TokenError('Invalid refresh token'));
+    }
+
+    req.refreshTokenInstance = refreshTokenInstance;
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+}
