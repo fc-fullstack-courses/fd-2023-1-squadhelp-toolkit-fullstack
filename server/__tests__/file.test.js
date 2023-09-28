6c7c1044019a0618c0f93a1e30eb3f1ca394b86d
Kyrylo <fresh.js.ftd.@gmail.com>
2023-09-28 15:24:52 +0300

    Add basic tests examples

---
+++ server/__tests__/file.test.js
@@ -0,0 +1,20 @@
+
+function sum(num1, num2) {
+  return Number(num1) + Number(num2);
+}
+
+test(`сумма 2 + 3 должна равнятся 5`, () => {
+  expect(sum(2, 3)).toBe(5);
+});
+
+test(`сумма '2' + '3' должна равнятся 5`, () => {
+  expect(sum('2', '3')).toBe(5);
+});
+
+test(`сумма 'asdsadsafffda' + '3' должна равнятся 5`, () => {
+  expect(sum('asdsadsafffda', '3')).toBe(NaN);
+});
+
+test(`сумма 0.1 + 0.2 должна равнятся 0.3`, () => {
+  expect(sum(0.1, 0.2)).toBeCloseTo(0.3);
+});
