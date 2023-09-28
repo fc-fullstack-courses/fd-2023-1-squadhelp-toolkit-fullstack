d35842470c8b620a7ee86fd142d7d22f213a5e07
Kyrylo <fresh.js.ftd.@gmail.com>
2023-09-28 14:36:23 +0300

    Prepare project for testing

---
+++ server/src/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+require('./dbMongo/mongoose');
+const router = require('./router');
+const handlerError = require('./handlerError/handler');
+const multerErrorHandler = require('./handlerError/multerErrorHandler');
+const tokenErrorsHandler = require('./handlerError/tokenErrorsHandler');
+const { FILES_PATH } = require('./constants');
+
+const createApp = () => {
+  const app = express();
+
+  app.use(cors());
+  app.use(express.json());
+  app.use('/public', express.static(FILES_PATH));
+  app.use(router);
+  
+  app.use(tokenErrorsHandler);
+  app.use(multerErrorHandler);
+  app.use(handlerError);
+
+  return app;
+}
+
+module.exports = createApp;
--- server/src/index.js
+++ server/src/index.js
@@ -1,25 +1,8 @@
 const http = require('http');
-const express = require('express');
-const cors = require('cors');
-require('./dbMongo/mongoose');
-const router = require('./router');
+const app = require('./app')();
 const controller = require('./socketInit');
-const handlerError = require('./handlerError/handler');
-const multerErrorHandler = require('./handlerError/multerErrorHandler');
-const tokenErrorsHandler = require('./handlerError/tokenErrorsHandler');
-const { FILES_PATH } = require('./constants');
 
-const PORT = process.env.PORT || 3000;
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-app.use('/public', express.static(FILES_PATH));
-app.use(router);
-
-app.use(tokenErrorsHandler);
-app.use(multerErrorHandler);
-app.use(handlerError);
+const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 server.listen(PORT,
